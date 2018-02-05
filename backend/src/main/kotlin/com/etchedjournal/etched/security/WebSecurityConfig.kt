34a76826364730310fcfa50b034c451c9c9d1966
Yaseen Kadir <yaseen95@users.noreply.github.com>
2018-02-05 20:01:13 +1100

    Temporary enabling CORS on backend
    Enabling CORS on backend for testing React app. Will disable it once building actual production version.

--- backend/src/main/kotlin/com/etchedjournal/etched/security/WebSecurityConfig.kt
+++ backend/src/main/kotlin/com/etchedjournal/etched/security/WebSecurityConfig.kt
@@ -1,10 +1,17 @@
 package com.etchedjournal.etched.security
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +25,28 @@ open class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .permitAll()
             .and()
                 .csrf().disable()
+    }
+
+    // TODO: Enable this only for local testing!
+    // This was enabled just to allow react to communicate with the backend locally
+    @Bean
+    open fun corsConfigurationSource(): CorsConfigurationSource {
+        // https://github.com/spring-projects/spring-boot/issues/5834#issuecomment-296370088
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+        configuration.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
+
+@Configuration
+open class WebConfig : WebMvcConfigurerAdapter() {
+    // TODO: Enable this only for local testing!
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
     }
 }
