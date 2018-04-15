e6d2c45e0490ffdd5f7dc4f26c8e4c143ac974ab
Yaseen Kadir <yaseen95@users.noreply.github.com>
2018-04-15 23:54:05 +1000

    Removed authentication tests
    Using keycloak drastically changes unit tests for auth. Removing them for now and will add mock stubs later. Proper testing will likely have to occur in integration tests.

--- backend/src/test/kotlin/com/etchedjournal/etched/EtchedApplicationTests.kt
+++ backend/src/test/kotlin/com/etchedjournal/etched/EtchedApplicationTests.kt
@@ -1,16 +1,21 @@
 package com.etchedjournal.etched
 
+import com.etchedjournal.etched.service.AuthService
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class EtchedApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @MockBean
+    private lateinit var authService: AuthService
+
+    @Test
+    fun contextLoads() {
+    }
 
 }
