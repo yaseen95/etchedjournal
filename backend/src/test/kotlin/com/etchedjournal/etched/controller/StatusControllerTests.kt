a7bdd5bc9cfef84555fb0ff91adbf7b3adb90159
Yaseen Kadir <yaseen95@users.noreply.github.com>
2018-05-16 09:30:33 +1000

    Fixed failing StatusControllerTests
    Have to specify TestConfig so that Mock AuthService is used.

--- backend/src/test/kotlin/com/etchedjournal/etched/controller/StatusControllerTests.kt
+++ backend/src/test/kotlin/com/etchedjournal/etched/controller/StatusControllerTests.kt
@@ -1,11 +1,13 @@
 package com.etchedjournal.etched.controller
 
+import com.etchedjournal.etched.TestConfig
 import org.hamcrest.Matchers.`is`
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@ContextConfiguration(classes = [TestConfig::class])
 class StatusControllerTests {
 
     private lateinit var mockMvc: MockMvc
