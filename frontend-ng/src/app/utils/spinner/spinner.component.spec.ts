d6ae40464112a4b4ba065404829d254920abd038
Yaseen Kadir <yaseenkadir@users.noreply.github.com>
2018-10-17 19:26:13 +1100

    Added spinner

---
+++ frontend-ng/src/app/utils/spinner/spinner.component.spec.ts
@@ -0,0 +1,39 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpinnerComponent } from './spinner.component';
+import TestUtils from '../test-utils.spec';
+
+describe('SpinnerComponent', () => {
+    let component: SpinnerComponent;
+    let fixture: ComponentFixture<SpinnerComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [SpinnerComponent]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SpinnerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('create without text', () => {
+        const textDe = TestUtils.queryExpectOne(fixture.debugElement, 'p');
+        expect((textDe.nativeElement as HTMLParagraphElement).textContent).toEqual('');
+    });
+
+    it('create with text', () => {
+        component.text = 'abc';
+        fixture.detectChanges();
+
+        const textDe = TestUtils.queryExpectOne(fixture.debugElement, 'p');
+        expect((textDe.nativeElement as HTMLParagraphElement).textContent).toEqual('abc');
+    });
+});
