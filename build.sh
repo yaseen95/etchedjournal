3ff3596d131c15f8684e93a410103b8c831b5b54
Yaseen Kadir <yaseenkadir@users.noreply.github.com>
2019-01-19 18:59:37 +1100

    Added dev flavor/environment to frontend

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export ETCHED_FLAVOR=dev
+
 ./gradlew nodeSetup && \
     ./gradlew frontend-ng:install && \
     ./gradlew frontend-ng:lint && \
---
+++ frontend-ng/src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    API_URL: 'https://etcheddev.com/api/v1',
+    auth: {
+        userPoolId: 'us-east-1_OjrfHbPY4',
+        userPoolWebClientId: '5sb8fjovam8pn5t8j4n10i2e79',
+    },
+    test: false,
+};
