1d78bd3f491cc8c697f742c0c551eeb24b8bac1b
Yaseen Kadir <yaseenkadir@users.noreply.github.com>
2019-02-21 09:28:02 +1100

    GH-131 Etch responses are ordered

--- backend/src/main/kotlin/com/etchedjournal/etched/service/impl/EtchServiceImpl.kt
+++ backend/src/main/kotlin/com/etchedjournal/etched/service/impl/EtchServiceImpl.kt
@@ -2,10 +2,8 @@ package com.etchedjournal.etched.service.impl
 
 import com.etchedjournal.etched.dto.EncryptedEntityRequest
 import com.etchedjournal.etched.models.OwnerType
-import com.etchedjournal.etched.models.jooq.generated.Tables
-import com.etchedjournal.etched.models.jooq.generated.tables.daos.EtchDao
 import com.etchedjournal.etched.models.jooq.generated.tables.pojos.Etch
-import com.etchedjournal.etched.models.jooq.generated.tables.records.EtchRecord
+import com.etchedjournal.etched.repository.EtchRepository
 import com.etchedjournal.etched.service.AuthService
 import com.etchedjournal.etched.service.EntryService
 import com.etchedjournal.etched.service.EtchService
@@ -13,7 +11,6 @@ import com.etchedjournal.etched.service.KeypairService
 import com.etchedjournal.etched.service.exception.BadRequestException
 import com.etchedjournal.etched.service.exception.NotFoundException
 import com.etchedjournal.etched.utils.id.IdGenerator
-import org.jooq.DSLContext
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -21,24 +18,23 @@ import java.time.Instant
 
 @Service
 class EtchServiceImpl(
-    private val etchDao: EtchDao,
+    private val etchRepo: EtchRepository,
     private val entryService: EntryService,
     private val authService: AuthService,
     private val idGenerator: IdGenerator,
-    private val keyPairService: KeypairService,
-    private val dslContext: DSLContext
+    private val keyPairService: KeypairService
 ) : EtchService {
 
     override fun getEtches(entryId: String): List<Etch> {
         // Defer to entryService.getEntry() to handle 404/permissions checks of Entry
         entryService.getEntry(entryId)
         logger.info("Getting etches for Entry(id={})", entryId)
-        return etchDao.fetchByEntryId(entryId)
+        return etchRepo.fetchByEntryId(entryId)
     }
 
     override fun getEtch(etchId: String): Etch {
         logger.info("Getting EtchEntity(id={})", etchId)
-        return etchDao.findById(etchId)
+        return etchRepo.findById(etchId)
             ?: throw NotFoundException("No EtchEntity with id $etchId exists.")
     }
 
@@ -55,28 +51,27 @@ class EtchServiceImpl(
         // Should we just return successfully or throw an error? If fails at the next step but
         // the error message isn't clear what the issue is
 
-        val newEtches = createRecords(etches, entryId)
-        newEtches.forEach { it.insert() }
-        // Can't use batchInsert due to version issues
-        // https://github.com/jOOQ/jOOQ/issues/8283
-        // dslContext.batchInsert(newEtches).execute()
-        return newEtches.map { it.into(Etch::class.java) }
+        val mappedEtches = mapRequestsToEtches(etches, entryId, etches[0].keyPairId)
+        return etchRepo.createEtches(mappedEtches)
     }
 
-    private fun createRecords(etchesReq: List<EncryptedEntityRequest>, entryId: String): List<EtchRecord> {
+    private fun mapRequestsToEtches(
+        etchesReq: List<EncryptedEntityRequest>,
+        entryId: String,
+        keyPairId: String
+    ): List<Etch> {
         val timestamp = Instant.now()
         return etchesReq.map {
-            val record = dslContext.newRecord(Tables.ETCH)
-            record.id = idGenerator.generateId()
-            // Give all the etches the same server side timestamp
-            // The client side schema will be responsible for ordering the etches
-            record.timestamp = timestamp
-            record.setContent(*it.content)
-            record.owner = authService.getUserId()
-            record.ownerType = OwnerType.USER
-            record.entryId = entryId
-            record.keyPairId = etchesReq[0].keyPairId
-            record
+            Etch(
+                idGenerator.generateId(),
+                timestamp,
+                it.content,
+                authService.getUserId(),
+                OwnerType.USER,
+                entryId,
+                keyPairId,
+                0
+            )
         }
     }
 
