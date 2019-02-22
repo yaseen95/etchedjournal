6d0f7d3e6fb464106c18b1d3541387ef52eebe7b
Yaseen Kadir <8746946+yaseenkadir@users.noreply.github.com>
2019-02-22 19:41:48 +1100

    GH-132 KeyPair responses are ordered (#139)

--- backend/src/test/kotlin/com/etchedjournal/etched/repository/KeyPairRepositoryTest.kt
+++ backend/src/test/kotlin/com/etchedjournal/etched/repository/KeyPairRepositoryTest.kt
@@ -1,11 +1,17 @@
 package com.etchedjournal.etched.repository
 
+import com.etchedjournal.etched.TestAuthService
 import com.etchedjournal.etched.TestConfig
+import com.etchedjournal.etched.TestRepoUtils
 import com.etchedjournal.etched.models.OwnerType
-import com.etchedjournal.etched.models.jooq.generated.tables.daos.KeyPairDao
 import com.etchedjournal.etched.models.jooq.generated.tables.pojos.KeyPair
+import com.etchedjournal.etched.utils.id.IdSerializer
+import org.jooq.exception.DataAccessException
 import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -22,7 +28,10 @@ import java.time.Instant
 class KeyPairRepositoryTest {
 
     @Autowired
-    private lateinit var dao: KeyPairDao
+    private lateinit var repo: KeyPairRepository
+
+    @Autowired
+    private lateinit var testRepoUtils: TestRepoUtils
 
     @Test
     fun `create and get`() {
@@ -35,16 +44,65 @@ class KeyPairRepositoryTest {
             OwnerType.USER,
             "foobar",
             10,
-            0
+            null
         )
-        dao.insert(keyPair)
+        repo.create(keyPair)
 
-        val retrieved = dao.findById("abcdefghijk")!!
+        val retrieved = repo.findById("abcdefghijk")!!
         assertEquals("abcdefghijk", retrieved.id)
         assertEquals(Instant.EPOCH, retrieved.timestamp)
         assertArrayEquals(byteArrayOf(1), retrieved.publicKey)
         assertArrayEquals(byteArrayOf(2), retrieved.privateKey)
         assertEquals("owner", retrieved.owner)
         assertEquals(OwnerType.USER, retrieved.ownerType)
+        assertEquals(10, retrieved.iterations)
+        assertEquals("foobar", retrieved.salt)
+        // version should have incremented
+        assertEquals(1, retrieved.version)
+    }
+
+    @Test
+    fun `throws when create already exists`() {
+        val keyPair = KeyPair(
+            IdSerializer.serialize(1),
+            Instant.EPOCH,
+            byteArrayOf(1),
+            byteArrayOf(2),
+            "owner",
+            OwnerType.USER,
+            "foobar",
+            10,
+            null
+        )
+        repo.create(keyPair)
+
+        try {
+            repo.create(keyPair)
+            fail()
+        } catch (e: DataAccessException) {
+            assertTrue(e.message!!.contains("Key (id)=(1) already exists."))
+        }
+    }
+
+    @Test
+    fun `findByOwner is sorted`() {
+        val id1 = IdSerializer.serialize(1)
+        val id2 = IdSerializer.serialize(2)
+        val id3 = IdSerializer.serialize(3)
+
+        // insert in reverse order
+        listOf(id3, id2, id1).map {
+            testRepoUtils.createKeyPair(id = it)
+        }
+
+        val fetched = repo.fetchByOwner(TestAuthService.TESTER_USER_ID)
+        val fetchedIds = fetched.map { it.id }
+        // Is this test robust enough?
+        assertEquals(listOf(id1, id2, id3), fetchedIds)
+    }
+
+    @Test
+    fun `returns null when no key pair with id exists`() {
+        assertNull(repo.findById(IdSerializer.serialize(100)))
     }
 }
