3de9b05208ff248520010c4e48cd6d97c12f8ea2
Yaseen Kadir <yaseenkadir@users.noreply.github.com>
2019-04-27 21:39:21 +1000

    Fixed formatting

--- backend/src/test/kotlin/com/etchedjournal/etched/utils/id/IncrementingIdGenerator.kt
+++ backend/src/test/kotlin/com/etchedjournal/etched/utils/id/IncrementingIdGenerator.kt
@@ -2,7 +2,7 @@ package com.etchedjournal.etched.utils.id
 
 import java.util.concurrent.atomic.AtomicLong
 
-open class IncrementingIdGenerator: IdGenerator {
+open class IncrementingIdGenerator : IdGenerator {
     private val currentId = AtomicLong(1)
 
     open override fun generateId(): String {
